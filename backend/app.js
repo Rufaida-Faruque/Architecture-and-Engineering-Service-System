54ff1cc847eea82baf189f4a1e28469aa53d9e9c
Rufaida-Faruque <rufaida.faruque@gmail.com>
2025-12-26 15:17:39 +0600

    testing deployment

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,4 @@
 
-// import express from 'express';   // Import express using ES module syntax
-// import dotenv from 'dotenv';    // Import dotenv to handle environment variables
-// import mongoose from 'mongoose'; // Import mongoose for MongoDB connection
-// import cors from 'cors';         // Import CORS to allow cross-origin requests
-// import authRoutes from './routes/auth.js'; // Import your authentication routes (ensure to include '.js' extension)
-
-
-// dotenv.config();  // Load environment variables from the .env file
-
-// const app = express();
-
-// // Enable CORS for all origins (you can limit this to a specific domain if needed)
-// app.use(cors());
-
-// // Middleware to parse incoming JSON requests
-// app.use(express.json());
-
-// // MongoDB connection
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log("MongoDB connection error:", err));
-
-// // Use the authentication routes (e.g., /api/auth/register)
-// app.use('/api/auth', authRoutes);
-
-// // Start the server on the specified port
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
-// import repoRoutes from "./routes/repository.js";
-// app.use("/api/repositories", repoRoutes);
-
-
-// import usersRoutes from "./routes/users.js";
-// app.use("/api/users", usersRoutes);
-
-// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
@@ -90,4 +50,13 @@ app.use("/api/bidSessions", bidSessionsRouter);
 
 import googleAuth from "./routes/google.js";
 app.use("/api/google", googleAuth);
+
+
+
+
+
+
+
+
+
 
