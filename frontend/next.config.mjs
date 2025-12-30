fe4e35b5e326056852ab2f9dea847e717188cc83
Rufaida-Faruque <138323439+Rufaida-Faruque@users.noreply.github.com>
2025-12-31 02:23:01 +0600

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+};
 
-// export default nextConfig;
-// next.config.mjs
-import { defineConfig } from 'next';
+export default nextConfig;
+next.config.mjs
 
-export default defineConfig({
-  // Tell Next.js to use the src directory for pages
-  experimental: {
-    srcDir: 'src'
-  },
-  reactStrictMode: true,
-});
