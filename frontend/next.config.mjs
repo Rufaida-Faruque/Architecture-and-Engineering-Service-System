7b011a2cb6cfa1cb95ca3dc10206148f01ce52b0
Rufaida-Faruque <138323439+Rufaida-Faruque@users.noreply.github.com>
2025-12-31 04:41:58 +0600

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,  // Enables React strict mode for better development experience
+  swcMinify: true,        // Enables SWC minifier for production builds
 };
 
 export default nextConfig;
-next.config.mjs
-
