306d2506a0f4c4430e115800235390f426968246
Rufaida-Faruque <138323439+Rufaida-Faruque@users.noreply.github.com>
2026-01-02 23:08:43 +0600

    Delete backend directory

--- backend/models/post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PostSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-}, { timestamps: true });
-
-export default mongoose.model("Post", PostSchema);
